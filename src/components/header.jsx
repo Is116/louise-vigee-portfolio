4084b0bbd28027319e690bb05dd3c3be239df85a
Isuru Pathirathna <isuru2002@gmail.com>
2023-08-28 11:26:40 +0530

    header init

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  const links = [
+    { text: 'Features', url: '#' },
+    { text: 'Marketplace', url: '#' },
+    { text: 'Company', url: '#' },
+    { text: 'Log in', url: '#' }
+  ];
 
-export default Header
+  return (
+    <header>
+      <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 md:px-8">
+        <div className="flex lg:flex-1">
+          <a href="/" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="flex md:hidden">
+            <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            >
+                <span className="sr-only">Open main menu</span>
+                <FontAwesomeIcon icon={faBars} className="h-6 w-6" />
+            </button>
+
+        </div>
+        <div className="hidden md:flex md:gap-x-12">
+          {links.map((link, index) => (
+            <a
+              key={index}
+              href={link.url}
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
+              {link.text}
+            </a>
+          ))}
+        </div>
+        <div className="hidden md:flex lg:flex-1 md:justify-end">
+          <a
+            href={links[3].url}
+            className="text-sm font-semibold leading-6 text-gray-900"
+          >
+            {links[3].text} <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
