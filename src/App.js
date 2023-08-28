f0241a32e88853ff11e85aeca1992ef3b6065bf1
Isuru Pathirathna <isuru2002@gmail.com>
2023-08-28 10:35:45 +0530

    init-complete

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Layout from './components/layout';
+import HomePage from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        hi
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HomePage = () => {
+    console.log('HomePage component rendering');
+  return (
+    <div>Home Page</div>
+  )
+}
+
+export default HomePage
